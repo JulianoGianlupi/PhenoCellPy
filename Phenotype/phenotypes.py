8c59756354cf843ba94e005a9b0fbed16016d40b
Juliano <julianoferrarigianlupi@gmail.com>
2022-09-28 11:32:08 -0400

    decstring

--- Phenotype/phenotypes.py
+++ Phenotype/phenotypes.py
@@ -5,28 +5,33 @@ from numpy.random import uniform
 
 
 class Phase:
-    """
-    Generic cell cycle phase class.
 
-    :param index:
-    :param previous_phase_index:
-    :param next_phase_index:
-    :param time_unit:
-    :param name:
-    :param division_at_phase_exit:
-    :param removal_at_phase_exit:
-    :param fixed_duration:
-    :param phase_duration:
-    :param entry_function:
-    :param exit_function:
-    :param arrest_function:
-    """
     def __init__(self, index: int = None, previous_phase_index: int = None, next_phase_index: int = None,
                  time_unit: str = "min", name: str = None, division_at_phase_exit: bool = False,
                  removal_at_phase_exit: bool = False, fixed_duration: bool = False, phase_duration: float = 10,
                  entry_function=None, entry_function_args: list = None, exit_function=None,
                  exit_function_args: list = None, arrest_function=None, arrest_function_args: list = None,
                  transition_to_next_phase=None):
+
+        """
+
+        :param index:
+        :param previous_phase_index:
+        :param next_phase_index:
+        :param time_unit:
+        :param name:
+        :param division_at_phase_exit:
+        :param removal_at_phase_exit:
+        :param fixed_duration:
+        :param phase_duration:
+        :param entry_function:
+        :param entry_function_args:
+        :param exit_function:
+        :param exit_function_args:
+        :param arrest_function:
+        :param arrest_function_args:
+        :param transition_to_next_phase:
+        """
 
         if index is None:
             self.index = 0  # int
@@ -68,6 +73,12 @@ class Phase:
             self.transition_to_next_phase = transition_to_next_phase
 
     def _transition_to_next_phase(self, dt):
+        """
+        Default stochastic phase transition function. Calculates a Poisson probability based on dt and
+        self.phase_duration, rolls a random number, and returns random number < probability
+        :param dt: float. Time-step length in the time units of Phase
+        :return: bool. random number < probability of transition
+        """
         prob = 1 - exp(dt/self.phase_duration)
         return uniform() < prob
 
@@ -75,7 +86,7 @@ class Phase:
         """
 
         :param dt: float. Time-step length in the time units of Phase
-        :return: tuple. First element of tuple bool denoting if the cell moves to the next phase. Second element
+        :return: tuple. First element of tuple: bool denoting if the cell moves to the next phase. Second element:
         denotes if the cell leaves the cell cycle and enters quiescence.
         """
 
