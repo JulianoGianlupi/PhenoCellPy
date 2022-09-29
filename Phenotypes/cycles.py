9b22e48f1a309c8618d367b85500fa9c45508227
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2022-09-29 15:07:20 -0400

    added missing call to entry function when entering a phase

--- Phenotypes/cycles.py
+++ Phenotypes/cycles.py
@@ -42,6 +42,8 @@ class Cycle:
         divides = self.current_phase.division_at_phase_exit
         dies = self.current_phase.removal_at_phase_exit
         self.set_phase(self.current_phase.next_phase_index)
+        if self.current_phase.entry_function is not None:
+            self.current_phase.entry_function()
         return True, dies, divides
 
     def set_phase(self, idx):
