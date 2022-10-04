8172c0ececeffae660739c3b5cff19953ec0d33e
Juliano <julianoferrarigianlupi@gmail.com>
2022-10-04 17:40:18 -0400

    docstring for Phase

--- Phenotypes/phases.py
+++ Phenotypes/phases.py
@@ -3,6 +3,104 @@ from numpy.random import uniform
 
 
 class Phase:
+    """
+    Base class to define phases of a cell cycle.
+
+    Methods
+    -------
+
+    time_step_phase()
+        Time steps the phase. Returns a tuple (did the cell transition to the next phase, did the cell enter
+        quiescence). See time_step_phase's documentation for further explanation.
+
+    transition_to_next_phase() or transition_to_next_phase(*args)
+        One of the default transition functions (`_transition_to_next_phase_deterministic`,
+        `_transition_to_next_phase_stochastic`) or an user defined function. If user defined, it will be called with
+        `transition_to_next_phase_args` as args. Must return a bool denoting if the transition occurs or not.
+
+    _transition_to_next_phase_deterministic()
+        Default deterministic transition function. Returns `time_in_phase > phase_duration`
+
+    _transition_to_next_phase_stochastic()
+        Default stochastic transition function. Probability of transition depends on `dt` and `phase_duration`
+
+    entry_function(*args)
+        Optional user defined function to be executed upon entering this phase. It gets called using attribute
+        `entry_function_args`. Must have no return
+
+    exit_function(*args)
+        Optional user defined function to be executed upon exiting this phase. It gets called using attribute
+        `exit_function_args`. Must have no return
+
+    arrest_function(*args)
+        Optional user defined function that returns true if the cell should exit the cell cycle and enter quiescence
+
+    update_volume() or update_volume(*args)
+        Function to update the volume of the cell. Can be user defined, in that case it has to use args and cannot have
+        a return value
+
+    Attributes
+    ----------
+
+    name : str
+        Name of the phase
+
+    index : int
+        The index of the phase in the cycle (0 idexed)
+
+    previous_phase_index : int
+        Index of the phase preceding this one (usually index-1)
+
+    next_phase_index : int
+        Index of the phase preceding this one (usually index+1)
+
+    time_unit : str
+        Time unit used by the model. TODO: defines unit conversions
+
+    dt : float
+        Size of the time-step in units of `time_unit`
+
+    division_at_phase_exit : bool
+        Flag for cell division at the end of the phase
+
+    removal_at_phase_exit : bool
+        Flag for cell removal (death) at the end of the phase
+
+    phase_duration : float
+        Average amount of time the phase lasts (in units of `time_unit`)
+
+    fixed_duration : bool
+        Flag for determining if the phase ends after a set amount of time or if it progresses to the next phase
+        in a stochastic manner with a rate `1/phase_duration`
+
+    time_in_phase : float
+        Time spent in this phase
+
+    volume : float
+        The volume of the cell in this phase
+
+    target_volume : float
+        The target volume of the cell in this phase
+
+    update_volume_rate : float
+        Amount of volume the cell will grow by if it is below the target and the default update volume function is used.
+
+    update_volume_args : list
+        Args used by custom `update_volume` function
+
+    entry_function_args : list
+        args for `entry_function`
+
+    exit_function_args : list
+        args for `exit_function`
+
+    arrest_function_args : list
+        args for `arrest_function`
+
+    transition_to_next_phase_args : list
+        args for `transition_to_next_phase` if a custom one is defined
+
+    """
 
     def __init__(self, index: int = None, previous_phase_index: int = None, next_phase_index: int = None,
                  dt: float = None, time_unit: str = "min", name: str = None, division_at_phase_exit: bool = False,
@@ -128,9 +226,11 @@ class Phase:
 
     def _transition_to_next_phase_stochastic(self):
         """
-        Default stochastic phase transition function. Calculates a Poisson probability based on dt and
-        self.phase_duration, rolls a random number, and returns random number < probability
-        :param args: float. Time-step length in the time units of Phase
+        Default stochastic phase transition function.
+
+        Calculates a Poisson probability based on dt and self.phase_duration (p=1-exp(-dt/phase_duration), rolls a
+        random number, and returns random number < probability.
+
         :return: bool. random number < probability of transition
         """
         prob = 1 - exp(-self.dt / self.phase_duration)
