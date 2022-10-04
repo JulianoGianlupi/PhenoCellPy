5b587b4d8fc9f1a3411d10e7ab55927c2ffddf59
Juliano <julianoferrarigianlupi@gmail.com>
2022-10-04 12:46:53 -0400

    testing Ki67Basic

--- Phenotypes/phenotypes.py
+++ Phenotypes/phenotypes.py
@@ -6,15 +6,18 @@
 #  - documentation
 #  - functions to attach to object
 
-from Phenotypes.cycles import SimpleLiveCycle
+import Phenotypes.cycles as cycles
 
 if __name__ == '__main__':
     pass
 
-    testCycle = SimpleLiveCycle()
-    for i in range(1000):
+    testCycle = cycles.Ki67Basic()
+    print(testCycle.time_in_cycle, testCycle.current_phase.name, testCycle.current_phase.time_in_phase)
+    for i in range(10000):
         changed_phase, died, divides = testCycle.time_step_cycle()
-        print(testCycle.time_in_cycle, testCycle.current_phase.name, testCycle.current_phase.time_in_phase)
+
         if changed_phase or died or divides:
             print(changed_phase, died, divides)
+            print(testCycle.time_in_cycle, testCycle.current_phase.name, testCycle.current_phase.time_in_phase)
+
     pass
