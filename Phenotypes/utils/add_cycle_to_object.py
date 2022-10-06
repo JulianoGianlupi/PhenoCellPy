a2ac11fa96aed0174b64827ff877608dcafea23d
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2022-10-06 15:02:41 -0400

    for now the cycle will be attached to an element of the cell dict. Added an utility function to do that. Changed CC3D model to use new implementation, also fixed a few things with the cc3d model

--- Phenotypes/utils/add_cycle_to_object.py
+++ Phenotypes/utils/add_cycle_to_object.py
@@ -4,7 +4,7 @@ from .. import cycles
 # from .. import phenotypes
 
 
-def add_cycle_to_object(o: object, cycle: str or cycles.cycle, name: str = "unnamed",
+def add_cycle_to_object(o: object, cycle: str or cycles.Cycle, name: str = "unnamed",
                         dt: float = 1, time_unit: str = "min", phases: list = None, quiescent_phase= None):
 
     if not hasattr(o, "__dict__"):
@@ -21,4 +21,24 @@ def add_cycle_to_object(o: object, cycle: str or cycles.cycle, name: str = "unna
         cycle = cycle(name=name, dt=dt, time_unit=time_unit, phases=phases, quiescent_phase=quiescent_phase)
 
     setattr(o, "cycle", cycle)
+
+
+def add_cycle_to_CC3D_cell(cell, cycle: str or cycles.Cycle, name: str = "unnamed", dt: float = 1,
+                           time_unit: str = "min", phases: list = None, quiescent_phase=None):
+
+    if not hasattr(cell, "dict"):
+        raise AttributeError("Cycle class is currently attached to the cell dictionary (i.e., cell.dict), however"
+                             f"object {cell} has no dict.")
+
+    if not isinstance(cycle, cycles.Cycle) and cycle not in cycles.cycle_names:
+        raise ValueError("Expected `cycle` parameter to be either an initialized instance of Cycle class or the name "
+                         "of a "
+                         f"pre-defined cycle. Got {cycle}")
+
+    if type(cycle) == str:
+        cycle = cycles.get_cycle_by_name(cycle)
+        cycle = cycle(name=name, dt=dt, time_unit=time_unit, phases=phases, quiescent_phase=quiescent_phase)
+
+    cell.dict["cycle"] = cycle
+
 
