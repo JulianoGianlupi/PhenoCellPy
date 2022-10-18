0843d606651fe50f4fdf1cb3fc6a3730a8e50041
Juliano <julianoferrarigianlupi@gmail.com>
2022-10-18 15:10:33 -0400

    somewhat working example. still needs work, the cells are all clumped

--- TF_examples/Ki67BasicCycle.py
+++ TF_examples/Ki67BasicCycle.py
@@ -3,6 +3,8 @@ import numpy as np
 
 import sys
 from os.path import abspath
+# import keyboard
+import time
 
 sys.path.extend([abspath("../")])  # todo: make this more refined
 
@@ -14,23 +16,29 @@ def get_radius_sphere(volume):
 
 
 # potential cutoff distance
-cutoff = 1
+cutoff = 3
 
 # space set up
-dim = [20, 20, 20]
-tf.init(dim=dim)
+dim = [50, 50, 50]
+tf.init(dim=dim, cutoff=cutoff)
 
-pot = tf.Potential.morse(d=0.1, a=6, min=-1, max=1)
+# pot = tf.Potential.morse(d=0.1, a=6, min=-1, max=1)
+pot = tf.Potential.glj(1)
 
 # Particle types
 
-volume = 20
+# volume = 10
 
-radius = get_radius_sphere(volume)
+mass = 40
 
-mass = volume
+radius = .4 #get_radius_sphere(volume)
 
-dt = 5  # min/time step
+global density
+density = mass / ((4/3)*np.pi*radius*radius*radius)
+
+
+
+dt = 1  # min/time step
 
 ki67_basic = pheno.cycles.Ki67Basic(dt=dt, target_volumes=[mass, mass], volumes=[mass, mass])
 
@@ -39,7 +47,7 @@ class CellType(tf.ParticleTypeSpec):
     mass = mass
     target_temperature = 0
     radius = radius
-    pvolume = volume
+    # volume = volume
     dynamics = tf.Overdamped
     cycle = ki67_basic
 
@@ -48,42 +56,64 @@ Cell = CellType.get()
 
 tf.bind.types(pot, Cell, Cell)
 
+
+rforce = tf.Force.random(mean=0, std=50)
+
+# bind it just like any other force
+tf.bind.force(rforce, Cell)
+
 first_cell = Cell([d // 2 for d in dim])
 first_cell.cycle = ki67_basic
+
+global cells_cycles
+
+cells_cycles = {f"{first_cell.id}": ki67_basic}
 
 
 def step_cycle_and_divide(event):
     # print("Time step")
     for p in Cell.items():
-        print("in particle loop")
-        print(hasattr(p, "cycle"))
-        p.cycle.current_phase.simulated_cell_volume = p.pvolume
-        phase_change, death, division = p.cycle.time_step_cycle()
-        print(phase_change, death, division)
-        if phase_change and len(CellType.items()) < 10:
+        print(p.id)
+        # print("in particle loop")
+        # print(hasattr(p, "volume"))
+        pcycle = cells_cycles[f"{p.id}"]
+        pcycle.current_phase.simulated_cell_volume = p.mass * density
+        phase_change, death, division = pcycle.time_step_cycle()
+        # print(pcycle.current_phase)
+        # print(pcycle.current_phase, phase_change, death, division, pcycle.current_phase.time_in_phase)
+        # print(1 - np.exp(-pcycle.current_phase.dt / pcycle.current_phase.phase_duration))
+        if phase_change and len(Cell.items()) < 10:
             print("@@@\nPHASE CHANGE\n@@@")
+            # time.sleep(1)
 
-        radius = get_radius_sphere(p.cycle.current_phase.volume)
+        radius = get_radius_sphere(pcycle.current_phase.volume)
 
         # book-keeping, making sure the simulated cell grows
         if p.radius < radius:
-            print("update volume")
+            # print("update volume")
             p.radius = radius
-            p.mass = p.cycle.current_phase.volume
-            p.volume = p.cycle.current_phase.volume
+            p.mass = ((4/3)*np.pi*radius*radius*radius) * density
+            # p.volume = p.cycle.current_phase.volume
 
         # if division occurs, divide
         if division:
             # save cell attribs to halve later
-            radius = p.radius
-            mass = p.mass
-            volume = p.pvolume
+            # radius = p.radius
+            cur_mass = p.mass
+            # volume = p.pvolume
 
             # divide and reasign attribs (is this step necessary?)
             child = p.split()
-            child.radius = p.radius = radius / 2
-            child.mass = p.mass = mass / 2
-            child.pvolume = p.pvolume = volume / 2
+
+            cells_cycles[f"{child.id}"] = ki67_basic
+
+            child.mass = p.mass = cur_mass / 2
+            child.radius = p.radius = get_radius_sphere((cur_mass / 2)/density)
+
+            cells_cycles[f"{child.id}"].volume = child.mass*density
+            cells_cycles[f"{child.id}"].simulated_cell_volume = child.mass*density
+
+            # child.pvolume = p.pvolume = volume / 2
     return 0
 
 
