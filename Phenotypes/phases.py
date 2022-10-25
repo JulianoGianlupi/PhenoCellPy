5e34649962fef0a07ed9ce2f405fab00d78ae985
JulianoGianlupi <julianoferrarigianlupi@gmail.com>
2022-10-25 12:00:07 -0400

    updated Ki67PositivePostMitotic

--- Phenotypes/phases.py
+++ Phenotypes/phases.py
@@ -475,15 +475,16 @@ class Ki67PositivePreMitotic(Ki67Positive):
 
 
 class Ki67PositivePostMitotic(Phase):
-    def __init__(self, index: int = 2, previous_phase_index: int = 1, next_phase_index: int = 0,
-                 dt: float = 0.1, time_unit: str = "min", name: str = "Ki 67+ post-mitotic",
-                 division_at_phase_exit: bool = True,
+    def __init__(self, index: int = 2, previous_phase_index: int = 1, next_phase_index: int = 0, dt: float = 0.1,
+                 time_unit: str = "min", name: str = "Ki 67+ post-mitotic", division_at_phase_exit: bool = True,
                  removal_at_phase_exit: bool = False, fixed_duration: bool = True, phase_duration: float = 2.5 * 60.0,
                  entry_function=None, entry_function_args: list = None, exit_function=None,
                  exit_function_args: list = None, arrest_function=None, arrest_function_args: list = None,
-                 transition_to_next_phase=None, transition_to_next_phase_args: list = None, target_volume: float = None,
-                 volume: float = None, update_volume=None, update_volume_args: list = None,
-                 update_volume_rate: float = None, simulated_cell_volume: float = None):
+                 transition_to_next_phase=None, transition_to_next_phase_args: list = None,
+                 simulated_cell_volume: float = None, cytoplasm_biomass_change_rate=None,
+                 nuclear_biomass_change_rate=None, calcification_rate=None, cytoplasm_volume=None,
+                 cytoplasm_target_volume=None, cytoplasm_target_fluid_fraction=None, nuclear_volume=None,
+                 nuclear_target_volume=None, nuclear_target_fluid_fraction=None, calcified_fraction=None):
 
         if entry_function is None:
             entry_function = self._standard_Ki67_positive_postmit_entry_function
@@ -500,7 +501,15 @@ class Ki67PositivePostMitotic(Phase):
                          exit_function_args=exit_function_args, arrest_function=arrest_function,
                          arrest_function_args=arrest_function_args, transition_to_next_phase=transition_to_next_phase,
                          transition_to_next_phase_args=transition_to_next_phase_args,
-                         simulated_cell_volume=simulated_cell_volume)
+                         simulated_cell_volume=simulated_cell_volume,
+                         cytoplasm_biomass_change_rate=cytoplasm_biomass_change_rate,
+                         nuclear_biomass_change_rate=nuclear_biomass_change_rate,
+                         calcification_rate=calcification_rate, cytoplasm_volume=cytoplasm_volume,
+                         cytoplasm_target_volume=cytoplasm_target_volume,
+                         cytoplasm_target_fluid_fraction=cytoplasm_target_fluid_fraction,
+                         nuclear_volume=nuclear_volume, nuclear_target_volume=nuclear_target_volume,
+                         nuclear_target_fluid_fraction=nuclear_target_fluid_fraction,
+                         calcified_fraction=calcified_fraction)
 
     def _standard_Ki67_positive_postmit_entry_function(self, *args):
         self.new_new_volume.target_cytoplasm /= 2
