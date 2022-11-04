fca97ac88a3419f6367e80960fcf035bae08641d
JulianoGianlupi <julianoferrarigianlupi@gmail.com>
2022-11-04 12:42:19 -0400

    example done

--- CC3D_examples/Apoptosys/Simulation/ApoptosysSteppables.py
+++ CC3D_examples/Apoptosys/Simulation/ApoptosysSteppables.py
@@ -6,7 +6,8 @@ from numpy import random as rng
 
 # sys.path.extend([abspath("../../..")])  # todo: make this more refined
 
-sys.path.extend(['D:\\modeling\\PhenoCellPy', 'D:/modeling/PhenoCellPy'])
+# sys.path.extend(['D:\\modeling\\PhenoCellPy', 'D:/modeling/PhenoCellPy'])
+sys.path.extend(['C:\\github\\PhenoCellPy', 'C:/github/PhenoCellPy'])
 
 import Phenotypes as pheno
 
@@ -22,7 +23,9 @@ class ApoptosysSteppable(SteppableBasePy):
         """
 
         self.side = 8  # cell side
-        self.dt = 5  # 5 min/MCS
+        self.dt = .5  # min/MCS
+
+        self.to_apoptose = 5
 
         for cell in self.cell_list:
             cell.targetVolume = self.side*self.side
@@ -34,26 +37,33 @@ class ApoptosysSteppable(SteppableBasePy):
         :param mcs: current Monte Carlo step
         """
 
-        if mcs == 50:  # we select a cell to undego apoptosis
-            self.selected_cell_id = rng.randint(0, len(self.cell_list)+1)
-            cell = self.fetch_cell_by_id(self.selected_cell_id)
-            cell.type = self.SELECTED
-            apopto = pheno.phenotypes.ApoptosisStandard(dt=self.dt, nuclear_fluid=[0], nuclear_solid=[0],
-                                                        cytoplasm_fluid=[.75*cell.volume],
-                                                        cytoplasm_solid=[(1-.75)*cell.volume],
-                                                        target_cytoplasm_to_nuclear_ratio=[0],
-                                                        simulated_cell_volume=[cell.volume])
+        if mcs == 50:  # we select cells to undergo apoptosis
+            self.selected_cell_ids = rng.randint(0, len(self.cell_list) + 1, self.to_apoptose, dtype=int)
+            for id in self.selected_cell_ids:
+                cell = self.fetch_cell_by_id(int(id))
+                cell.type = self.SELECTED
+                cell.lambdaVolume = 25
+                apopto = pheno.phenotypes.ApoptosisStandard(dt=self.dt, nuclear_fluid=[0], nuclear_solid=[0],
+                                                            cytoplasm_fluid=[.75*cell.volume],
+                                                            cytoplasm_solid=[(1-.75)*cell.volume],
+                                                            target_cytoplasm_to_nuclear_ratio=[0],
+                                                            simulated_cell_volume=[cell.volume])
 
-            pheno.utils.add_phenotype_to_CC3D_cell(cell, apopto)
+                pheno.utils.add_phenotype_to_CC3D_cell(cell, apopto)
+                changed_phase, died, divides = cell.dict["phenotype"].time_step_phenotype()
 
         if mcs > 50:
-            cell = self.fetch_cell_by_id(self.selected_cell_id)
-            print(cell.dict["phenotype"].current_phase.volume.cytoplasm_solid_target,
-                  cell.dict["phenotype"].current_phase.volume.nuclear_solid_target,
-                  cell.dict["phenotype"].current_phase.volume.target_fluid_fraction,
-                  cell.dict["phenotype"].current_phase.volume.total)
-            cell.targetVolume = cell.dict["phenotype"].current_phase.volume.total
-            cell.dict["phenotype"].current_phase.simulated_cell_volume = cell.volume
+            for id in self.selected_cell_ids:
+                cell = self.fetch_cell_by_id(int(id))
+                if cell is not None:
+                    changed_phase, died, divides = cell.dict["phenotype"].time_step_phenotype()
+                    print(cell.dict["phenotype"].current_phase.volume.cytoplasm_solid_target,
+                          cell.dict["phenotype"].current_phase.volume.nuclear_solid_target,
+                          cell.dict["phenotype"].current_phase.volume.target_fluid_fraction,
+                          cell.dict["phenotype"].current_phase.volume.total,
+                          cell.volume)
+                    cell.targetVolume = cell.dict["phenotype"].current_phase.volume.total
+                    cell.dict["phenotype"].current_phase.simulated_cell_volume = cell.volume
 
     def finish(self):
         """
