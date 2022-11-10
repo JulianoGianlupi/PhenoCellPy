748259f2cac69424e9d0ac68774e482a0073e889
JulianoGianlupi <julianoferrarigianlupi@gmail.com>
2022-11-10 12:19:05 -0500

    added todo idea
    Signed-off-by: JulianoGianlupi <julianoferrarigianlupi@gmail.com>

--- Phenotypes/phenotypes.py
+++ Phenotypes/phenotypes.py
@@ -9,6 +9,10 @@ from numpy.random import randint
 #  - switch how defalts are handled to be like phases.NecrosisLysed
 #  - add copyright
 #  - fix _check_arguments
+#  - add cell heterogeneity. have (yet another) argument to set randomization to True/False (and possibly which distri-
+#    tion to use, log-normal, normal, etc). If true, rates, durations, and volumes are slightly shuffled. Also have
+#    an argument to set the variance
+
 
 def _check_arguments(number_phases, cycle_name, division_at_phase_exits, removal_at_phase_exits, fixed_durations,
                      phase_durations, entry_functions, entry_functions_args, exit_functions, exit_functions_args,
