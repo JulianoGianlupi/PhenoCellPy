e2d6436071119943874de8d050051cefa8d10ad5
JulianoGianlupi <jferrarigian@unither.com>
2025-08-29 10:44:57 -0400

    Squashed commit of the following:
    commit 90fb1a6f7ff564e439535b57887bcb5822456ece
    Author: JulianoGianlupi <jferrarigian@unither.com>
    Date:   Fri Aug 29 10:44:33 2025 -0400
    
        reformat file
    
    commit b740a8afb5f087bed63f2e0a13f55d1907a96ee0
    Author: Michael-Getz <31327120+Michael-Getz@users.noreply.github.com>
    Date:   Thu Aug 28 20:09:43 2025 -0700
    
        initial branch commit
    
        initial changes to phases to calculate rates from user parameters. fixes to the basic cycle improved to use correct rate. fixes to cell division in the cc3d example problem.

--- CC3D_examples/Ki67BasicCycle_ImprovedDivision/Simulation/Ki67CycleSteppables.py
+++ CC3D_examples/Ki67BasicCycle_ImprovedDivision/Simulation/Ki67CycleSteppables.py
@@ -68,19 +68,29 @@ class ConstraintInitializerSteppable(SteppableBasePy):
         cell = self.new_cell(self.CELL)
         self.cell_field[x:x + side, y:y + side, 0] = cell
 
-        dt = 5  # 5 min/mcs
-
-        ki67_basic_modified_transition = pcp.phenotypes.Ki67Basic(dt=dt,
+        dt = 1  # 5 min/mcs
+        #ki67_basic_modified_transition = pcp.phenotypes.Ki67Basic(dt=dt, 
+        #                                                          check_transition_to_next_phase_functions=[None,
+        #                                                                                                      Ki67pos_transition],
+        #                                                          check_transition_to_next_phase_functions_args=[None,
+        #                                                                                                           [-9, 1, -9, 1]])
+        
+        #notes on the below modification, right now fluid_change_rate may not be doing anything (always overwrites with 1 or the previous derived
+        #value). Likely the change rates can be applied to all phases, though it will not change anything.
+        ki67_basic_modified_transition = pcp.phenotypes.Ki67Basic(dt=dt, nuclear_volume_change_rate=[None,0.0055], 
+                                                                  cytoplasm_volume_change_rate=[None,0.0045],
+                                                                  fluid_change_rate=[None,0.05],
                                                                   check_transition_to_next_phase_functions=[None,
                                                                                                               Ki67pos_transition],
                                                                   check_transition_to_next_phase_functions_args=[None,
                                                                                                                    [-9, 1, -9, 1]])
-
+#second arg makes sure volume is made before progression - with small lambda and large steps divergence is observed. Possible to 
+#have phenocell read volume so these arguments dont need to be passed? Check issues on Github to make sure it makes sense!
         self.volume_conversion_unit = self.target_volume / ki67_basic_modified_transition.current_phase.volume.total
 
         for cell in self.cell_list:
             cell.targetVolume = self.target_volume
-            cell.lambdaVolume = 2.0
+            cell.lambdaVolume = 20.0
             pcp.utils.add_phenotype_to_CC3D_cell(cell, ki67_basic_modified_transition)
             cell.dict["phase_index_plus_1"] = cell.dict["phenotype"].current_phase.index + 1
 
@@ -95,7 +105,7 @@ class MitosisSteppable(MitosisSteppableBase):
 
         self.previous_number_cells = 0
 
-        self.plot = False
+        self.plot = True
         self.save = False
 
         if self.save:
@@ -169,13 +179,14 @@ class MitosisSteppable(MitosisSteppableBase):
         n_one = 0
 
         volumes = []
+        Targetvolumes = []
 
         time_spent_in_0 = []
         time_spent_in_1 = []
 
         for cell in self.cell_list:
             volumes.append(cell.volume)
-            cell.dict["phenotype"].current_phase.simulated_cell_volume = cell.volume
+            #cell.dict["phenotype"].current_phase.simulated_cell_volume = cell.volume #This passes in, but is unused
             # print(len(args))
             if cell.dict["phenotype"].current_phase.index == 0:
                 n_zero += 1
@@ -183,7 +194,7 @@ class MitosisSteppable(MitosisSteppableBase):
             elif cell.dict["phenotype"].current_phase.index == 1:
                 n_one += 1
                 time_spent_in_1.append(cell.dict["phenotype"].current_phase.time_in_phase)
-                # args = [cc3d cell volume, doubling colume, time in phase, phase duration
+                # args = [cc3d cell volume, doubling colume, time in phase, phase duration]
                 args = [
                     cell.volume,
                     .9 * self.constraint_vars.doubling_volume,  # we use 90% of the doubling volume because cc3d cells
@@ -194,10 +205,6 @@ class MitosisSteppable(MitosisSteppableBase):
                 cell.dict["phenotype"].current_phase.check_transition_to_next_phase_function_args = args
 
             changed_phase, should_be_removed, divides = cell.dict["phenotype"].time_step_phenotype()
-            converted_volume = self.constraint_vars.volume_conversion_unit * \
-                               cell.dict["phenotype"].current_phase.volume.total
-
-            cell.targetVolume = converted_volume
 
             if changed_phase:
                 cell.dict["phase_index_plus_1"] = cell.dict["phenotype"].current_phase.index + 1
@@ -208,6 +215,10 @@ class MitosisSteppable(MitosisSteppableBase):
                 if len(self.cell_list) < 10:
                     print(f"@@@\nCELL DIVISION\n@@@\ncell volume={cell.volume}")
                 cells_to_divide.append(cell)
+            converted_volume = self.constraint_vars.volume_conversion_unit * \
+                               cell.dict["phenotype"].current_phase.volume.total
+            cell.targetVolume = converted_volume
+            #Targetvolumes.append(cell.targetVolume)
 
         if self.save or self.plot:
             volume_median = median(volumes)
@@ -294,6 +305,8 @@ class MitosisSteppable(MitosisSteppableBase):
 
     def update_attributes(self):
         # resetting target volume
+        self.parent_cell.dict["phenotype"].current_phase.volume.nuclear_solid = self.parent_cell.dict["phenotype"].current_phase.volume.nuclear_solid / 2
+        self.parent_cell.dict["phenotype"].current_phase.volume.cytoplasm_solid = self.parent_cell.dict["phenotype"].current_phase.volume.cytoplasm_solid / 2
         converted_volume = self.constraint_vars.volume_conversion_unit * \
                            self.parent_cell.dict["phenotype"].current_phase.volume.total
         self.parent_cell.targetVolume = converted_volume
--- phenocellpy/phases.py
+++ phenocellpy/phases.py
@@ -31,6 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 from numpy import exp
+from numpy import log
 from numpy.random import uniform
 
 from phenocellpy.cell_volume import CellVolumes
@@ -855,7 +856,9 @@ class Ki67Positive(Phase):
         check_transition_to_next_phase_function_args: list = None,
         simulated_cell_volume: float = None,
         cytoplasm_volume_change_rate=None,
+        cytoplasm_doubling_duration=None,
         nuclear_volume_change_rate=None,
+        nuclear_doubling_duration=None,
         calcification_rate=None,
         target_fluid_fraction=None,
         nuclear_fluid=None,
@@ -870,7 +873,7 @@ class Ki67Positive(Phase):
         relative_rupture_volume=None,
         user_phase_time_step=None,
         user_phase_time_step_args=None
-    ):
+    ):  #TODO, create entries such that the new args are in the base phase class
 
         if entry_function is None:
             entry_function = self._double_target_volume
@@ -896,42 +899,38 @@ class Ki67Positive(Phase):
                 "Expected "
                 f"list or tuple got {type(exit_function_args)}"
             )
-        if cytoplasm_volume_change_rate is None and cytoplasm_fluid is not None and cytoplasm_solid is not None:
-            cytoplasm_volume_change_rate = (cytoplasm_fluid + cytoplasm_solid) / (phase_duration / dt)
 
-        elif cytoplasm_volume_change_rate is None and cytoplasm_fluid is not None:
-            cytoplasm_volume_change_rate = cytoplasm_fluid / (phase_duration / dt)
-
-        elif cytoplasm_volume_change_rate is None and cytoplasm_solid is not None:
-            cytoplasm_volume_change_rate = cytoplasm_solid / (phase_duration / dt)
-
+        if cytoplasm_volume_change_rate is None and cytoplasm_doubling_duration is not None:  #propose adding an argument to allow different phase times that diverge from the total phase time. Do we want to build in safeties, i.e cytoplasm_doubling_duration>phasetime?
+            cytoplasm_volume_change_rate = -np.log(0.05) / cytoplasm_doubling_duration
         elif cytoplasm_volume_change_rate is None:
-            cytoplasm_volume_change_rate = 1
+            cytoplasm_volume_change_rate = 0.27 / 60.0  # default to physicell rates
         else:
             cytoplasm_volume_change_rate = cytoplasm_volume_change_rate
 
-        if nuclear_volume_change_rate is None and cytoplasm_fluid is not None and cytoplasm_solid is not None:
-            nuclear_volume_change_rate = (nuclear_fluid + nuclear_solid) / (phase_duration / dt)
-
-        elif nuclear_volume_change_rate is None and cytoplasm_fluid is not None:
-            nuclear_volume_change_rate = nuclear_fluid / (phase_duration / dt)
-
-        elif nuclear_volume_change_rate is None and cytoplasm_solid is not None:
-            nuclear_volume_change_rate = nuclear_solid / (phase_duration / dt)
-
+        if nuclear_volume_change_rate is None and nuclear_doubling_duration is not None:  #propose adding an argument to allow different phase times that diverge from the total phase time. Do we want to build in safeties, i.e cytoplasm_doubling_duration>phasetime?
+            nuclear_volume_change_rate = -np.log(0.05) / nuclear_doubling_duration
         elif nuclear_volume_change_rate is None:
-            nuclear_volume_change_rate = 1
+            nuclear_volume_change_rate = 0.33 / 60.0
         else:
             nuclear_volume_change_rate = nuclear_volume_change_rate
 
-        if fluid_change_rate is None and cytoplasm_fluid is not None and nuclear_fluid is not None:
-            fluid_change_rate = (cytoplasm_fluid + nuclear_fluid) / (phase_duration / dt)
-        elif fluid_change_rate is None and cytoplasm_fluid is not None:
-            fluid_change_rate = cytoplasm_fluid / (phase_duration / dt)
-        elif fluid_change_rate is None and nuclear_fluid is not None:
-            fluid_change_rate = nuclear_fluid / (phase_duration / dt)
+        if fluid_change_rate is None and cytoplasm_doubling_duration is not None and nuclear_doubling_duration is not None:
+            if cytoplasm_doubling_duration < nuclear_doubling_duration:  #with custom rates we follow the assumption that the fluid intake rate is an order of magnitude faster then the fastest of the two rates
+                fluid_change_rate = -np.log(0.05) / (cytoplasm_doubling_duration / 10)
+            else:
+                fluid_change_rate = -np.log(0.05) / (nuclear_doubling_duration / 10)
+        elif fluid_change_rate is None and cytoplasm_doubling_duration is not None:
+            fluid_change_rate = -np.log(0.05) / (
+                cytoplasm_doubling_duration / 10
+            )  #with custom rates we follow the assumption that the fluid intake rate is an order of magnitude faster
+        elif fluid_change_rate is None and nuclear_doubling_duration is not None:
+            fluid_change_rate = -np.log(0.05) / (
+                cytoplasm_doubling_duration / 10
+            )  #with custom rates we follow the assumption that the fluid intake rate is an order of magnitude faster
+        elif fluid_change_rate is None:
+            fluid_change_rate = 3.0 / 60.0  # default to physicell rates
         else:
-            fluid_change_rate = 1
+            fluid_change_rate = fluid_change_rate  #why did we previously not allow use defined fluid rate?
 
         super().__init__(
             index=index,
@@ -955,7 +954,9 @@ class Ki67Positive(Phase):
             check_transition_to_next_phase_function_args=check_transition_to_next_phase_function_args,
             simulated_cell_volume=simulated_cell_volume,
             cytoplasm_volume_change_rate=cytoplasm_volume_change_rate,
+            cytoplasm_doubling_duration=cytoplasm_doubling_duration,
             nuclear_volume_change_rate=nuclear_volume_change_rate,
+            nuclear_doubling_duration=nuclear_doubling_duration,
             calcification_rate=calcification_rate,
             target_fluid_fraction=target_fluid_fraction,
             nuclear_fluid=nuclear_fluid,
